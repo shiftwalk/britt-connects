c3d797c1f409050b2e89f1cf3111ef6e9eb637e3
Sam Goddard <hello@samgoddard.co.uk>
2022-04-27 12:16:16 +0100

    further templating~]

--- components/footer.js
+++ components/footer.js
@@ -2,28 +2,38 @@ import Container from '@/components/container'
 
 export default function Footer() {
   return (
-    <footer className="mb-4">
-      <Container>
-        <div className="border-t border-black py-4">
-          <div className="flex flex-wrap text-xs">
-            <div className="flex space-x-1 mb-1 md:mb-0">
-              <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-500 focus:text-gray-500">MIT License</a>
+    <footer className="p-3 md:p-4 xl:p-6">
+      <div className="bg-yellow w-full rounded-full text-off-black py-3 px-6 text-sm md:text-base xl:text-lg">
+        <div className="flex flex-wrap">
+          <div className="flex space-x-1 mb-1 md:mb-0 justify-center md:justify-start w-full md:w-auto">
+            <span className="block">&copy; Copyright { new Date().getFullYear()}, BrittConnects</span>
+          </div>
 
-              <span className="block">&bull;</span>
+          <div className="w-full md:w-auto flex space-x-2 lg:space-x-3 mb-1 md:mb-0 mx-auto items-center justify-center md:justify-start">
+            <span className="block"><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="underline">Instagram</a></span>
 
-              <span className="block"><a href="https://github.com/samuelgoddard/next-tailwind-motion" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-500 focus:text-gray-500">Github</a></span>
+            <span className="block w-[7px] h-[7px] rounded-full border border-off-black"></span>
 
-              <span className="block">&bull;</span>
+            <span className="block"><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="underline">LinkedIn</a></span>
 
-              <span className="block"><a href="https://twitter.com/samuelgoddard" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-500 focus:text-gray-500">Twitter</a></span>
-            </div>
+            <span className="block w-[7px] h-[7px] rounded-full border border-off-black"></span>
 
-            <div className="md:ml-auto w-full md:w-auto flex space-x-1">
-              <span className="block">Maintained by <a href="https://samgoddard.co.uk" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-500 focus:text-gray-500">Sam Goddard</a></span>
-            </div>
+            <span className="block"><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="underline">Email</a></span>
+
+            <span className="block md:hidden w-[7px] h-[7px] rounded-full border border-off-black"></span>
+
+            <span className="block md:hidden"><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="underline">Terms</a></span>
+          </div>
+
+          <div className="space-x-3 mb-1 md:mb-0 mx-auto items-center hidden md:flex">
+            <span className="block"><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="underline">Terms</a></span>
+          </div>
+
+          <div className="md:ml-auto w-full md:w-auto flex space-x-1 justify-center md:justify-start">
+            <span className="block">Site by <a href="https://shiftwalk.studio" target="_blank" rel="noopener noreferrer" className="underline">ShiftWalk</a></span>
           </div>
         </div>
-      </Container>
+      </div>
     </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -197,10 +197,49 @@ export default function Home() {
                     </li>
                   </ul>
                 </section>
-              </m.div>
-            </m.main>
 
-            {/* <Footer /> */}
+                <section className="p-3 md:p-4 xl:p-6 pt-[20vw] md:pt-[16vw] xl:pt-[16vw]">
+                  <div className="relative flex justify-center">
+                    <div className="px-[3vw] md:px-[10vw] flex flex-wrap items-center justify-center">
+                      <div className="w-full">
+                        <span className="block mx-auto text-center mb-6 md:mb-12">(Kind words)</span>
+                        <h3 className="text-[9vw] md:text-[5.6vw] font-display uppercase leading-[0.92] md:leading-[0.92] text-center relative z-10 mix-blend-lighten w-full mb-12 md:mb-12">“Britt has an incredible knack of reading between the lines to find perfect candidates”</h3>
+                        <span className="block mx-auto text-center uppercase text-lg leading-tight md:text-xl md:leading-tight">Kelly Wu<span className="block">Recruiting Lead, Google</span></span>
+                      </div>
+                    </div>
+
+                    <div className="opacity-25 absolute inset-0 z-0 w-full h-full flex items-center justify-center">
+                      <div className="w-10/12 md:w-5/12">
+                        <svg className="w-full blur-[6px] md:blur-[0.8vw]" viewBox="0 0 58 59" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28.358 58.706h.637V29.442h-.637v29.264Z" fill="#DEE212"/><path d="m24.331 58.33.631.09 4.035-28.98-.63-.09-4.036 28.98Z" fill="#DEE212"/><path d="m20.394 57.395.612.177 7.99-28.13-.612-.177-7.99 28.13Z" fill="#DEE212"/><path d="m16.627 55.915.582.261L29 29.443l-.582-.262-11.79 26.734Z" fill="#DEE212"/><path d="m13.09 53.922.54.34L28.99 29.446l-.54-.34L13.09 53.922Z" fill="#DEE212"/><path d="m9.874 51.445.488.413 18.634-22.417-.488-.413L9.874 51.445Z" fill="#DEE212"/><path d="m7.022 48.541.427.478 21.543-19.581-.426-.478L7.022 48.54Z" fill="#DEE212"/><path d="m4.611 45.235.357.534 24.06-16.388-.356-.534-24.06 16.388Z" fill="#DEE212"/><path d="m2.655 41.691.28.578L28.99 29.442l-.28-.578L2.656 41.691Z" fill="#DEE212"/><path d="m1.23 37.866.197.612 27.57-9.043-.197-.612-27.57 9.043Z" fill="#DEE212"/><path d="m.335 33.892.11.632 28.548-5.081-.11-.633L.334 33.892Z" fill="#DEE212"/><path d="m0 29.818.022.642 28.972-1.021-.023-.642L0 29.817Z" fill="#DEE212"/><path d="m.228 25.74-.066.639 28.83 3.057.067-.639L.229 25.74Z" fill="#DEE212"/><path d="m1.02 21.736-.154.624 28.128 7.08.154-.624-28.128-7.08Z" fill="#DEE212"/><path d="m2.35 17.881-.24.596L28.99 29.44l.238-.596L2.35 17.88Z" fill="#DEE212"/><path d="m4.205 14.25-.318.556 25.105 14.632.319-.557L4.205 14.25Z" fill="#DEE212"/><path d="m6.539 10.914-.392.507L28.99 29.438l.393-.507L6.539 10.914Z" fill="#DEE212"/><path d="m9.313 7.942-.458.447 20.138 21.05.458-.446L9.313 7.943Z" fill="#DEE212"/><path d="m12.468 5.386-.515.378 17.04 23.675.515-.378-17.04-23.675Z" fill="#DEE212"/><path d="m15.948 3.293-.563.302 13.61 25.838.563-.302-13.61-25.838Z" fill="#DEE212"/><path d="m19.677 1.719-.598.22 9.915 27.498.598-.22L19.677 1.72Z" fill="#DEE212"/><path d="m23.591.678-.623.134 6.027 28.624.623-.134L23.591.678Z" fill="#DEE212"/><path d="m27.613.199-.636.045 2.024 29.192.636-.045L27.613.2Z" fill="#DEE212"/><path d="m31.658.29-.636-.045L29 29.437l.636.045L31.658.29Z" fill="#DEE212"/><path d="m35.646.948-.623-.134-6.028 28.624.624.134L35.646.948Z" fill="#DEE212"/><path d="m39.513 2.158-.599-.22L29 29.437l.599.22 9.915-27.5Z" fill="#DEE212"/><path d="m43.17 3.902-.562-.302-13.61 25.839.563.302 13.61-25.839Z" fill="#DEE212"/><path d="m46.55 6.138-.516-.378-17.04 23.675.516.378L46.55 6.138Z" fill="#DEE212"/><path d="m49.595 8.835-.459-.446L29 29.439l.458.447 20.138-21.05Z" fill="#DEE212"/><path d="m52.234 11.93-.392-.507-22.844 18.016.392.507 22.844-18.017Z" fill="#DEE212"/><path d="m54.39 15.53-.32-.56-25.22 14.632.319.56 25.22-14.632Z" fill="#DEE212"/><path d="m56.12 19.072-.239-.596-26.878 10.962.238.596L56.12 19.072Z" fill="#DEE212"/><path d="m57.283 22.98-.154-.623L29 29.437l.154.623 28.128-7.08Z" fill="#DEE212"/><path d="m57.895 27.013-.067-.639-28.83 3.057.066.639 28.83-3.057Z" fill="#DEE212"/><path d="m57.95 31.1.022-.641L29 29.438l-.022.642 28.971 1.02Z" fill="#DEE212"/><path d="m57.44 35.154.11-.633-28.547-5.08-.11.632 28.547 5.081Z" fill="#DEE212"/><path d="m56.369 39.091.197-.611-27.57-9.043-.198.611 27.57 9.043Z" fill="#DEE212"/><path d="m54.778 42.847.279-.578-26.055-12.828-.28.578 26.056 12.828Z" fill="#DEE212"/><path d="m52.68 46.334.355-.533-24.033-16.364-.356.533L52.68 46.334Z" fill="#DEE212"/><path d="m50.115 49.5.426-.479L28.998 29.44l-.426.478 21.543 19.581Z" fill="#DEE212"/><path d="m47.146 52.273.488-.414L29 29.442l-.488.413 18.634 22.418Z" fill="#DEE212"/><path d="m43.82 54.602.54-.341L29 29.444l-.54.34L43.82 54.603Z" fill="#DEE212"/><path d="m40.21 56.442.583-.262-11.791-26.733-.582.261 11.79 26.734Z" fill="#DEE212"/><path d="m36.377 57.749.612-.178-7.99-28.13-.613.178 7.99 28.13Z" fill="#DEE212"/><path d="m32.398 58.512.631-.09-4.035-28.979-.63.09 4.034 28.979Z" fill="#DEE212"/></svg>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+
+                <section className="p-3 md:p-4 xl:p-6 pt-[20vw] md:pt-[15vw] xl:pt-[15vw]">
+                  <h2 className="text-[14vw] md:text-[8.4vw] font-display uppercase leading-[0.9] md:leading-[0.9] text-center relative z-10 mix-blend-lighten pt-[2vw] pb-[6vw]">Get <span className="block">Started</span></h2>
+
+                  <div className="flex flex-wrap mb-[3vw]">
+                    <div className="w-full md:w-1/2 flex justify-center border-b md:border-b-0 md:border-r border-yellow">
+                      <div className="max-w-[300px] text-center py-[5vw]">
+                        <a href="#" className="inline-block uppercase tracking-tight leading-none md:leading-none xl:leading-none text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl ml-auto px-2 2xl:px-3 pt-[3px] pb-[2px] rounded-full border-yellow border hover:bg-yellow hover:text-off-black focus:bg-yellow focus:text-off-black focus:outline-none ring-offset-4 ring-offset-off-black focus:ring-[2px] ring-yellow mb-8">Clients</a>
+
+                        <p className="">High-quality talent is your secret to success, not an arbitrary expense. We’re here to ensure that your investment has the greatest ROI based on your company’s needs and culture.</p>
+                      </div>
+                    </div>
+                    <div className="w-full md:w-1/2 flex justify-center">
+                      <div className="max-w-[300px] text-center py-[5vw]">
+                        <a href="#" className="inline-block uppercase tracking-tight leading-none md:leading-none xl:leading-none text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl ml-auto px-2 2xl:px-4 pt-[3px] pb-[2px] rounded-full border-yellow border hover:bg-yellow hover:text-off-black focus:bg-yellow focus:text-off-black focus:outline-none ring-offset-4 ring-offset-off-black focus:ring-[2px] ring-yellow mb-8">Talent</a>
+
+                        <p className="">High-quality talent is your secret to success, not an arbitrary expense. We’re here to ensure that your investment has the greatest ROI based on your company’s needs and culture.</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </m.div>
+              
+              <Footer />
+            </m.main>
           </SmoothWrapper>
         </div>
       </LazyMotion>
