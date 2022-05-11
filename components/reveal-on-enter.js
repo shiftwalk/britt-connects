8eb39d590989cc200589f64b58d6ebde8cd4f01a
Sam Goddard <hello@samgoddard.co.uk>
2022-05-11 16:07:12 +0100

    reveal on enter

---
+++ components/reveal-on-enter.js
@@ -0,0 +1,21 @@
+import { m } from 'framer-motion'
+
+export default function RevealOnEnter({ children, duration, className, delay }) {
+  return(
+    <div className={`relative overflow-hidden ${className}`}>
+      <m.div
+        initial="none"
+        whileInView="block"
+        viewport={{ once: true, margin: '50px' }}
+        transition={{ duration: duration ? duration : 0.5, ease: [0.83, 0, 0.17, 1], delay: delay ? delay : 0 }}
+        className="w-full"
+        variants={{
+          block: { y: 0 },
+          none: { y: '100%' }
+        }}
+      >
+        {children}
+      </m.div>
+    </div>
+  )
+}
