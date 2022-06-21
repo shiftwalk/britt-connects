789fd81cc4aa99dd34618ecc2d867cd699101bd4
Sam Goddard <hello@samgoddard.co.uk>
2022-06-21 12:17:44 +0100

    social + favicon|

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,12 +1,20 @@
 export default {
   defaultTitle: 'Connecting Humans | Britt Connects',
-  description: 'A simple Next.js starter kit with Tailwind and Framer Motion.',
+  description: 'We connect top-tier talent with high-caliber opportunities. We care deeply about our talent and partner only with clients who feel the same, meaning we value personal connections over numbers and transactions.',
   titleTemplate: '%s | Britt Connects',
   openGraph: {
     type: 'website',
     locale: 'en_GB',
     url: 'https://sg-next-tailwind-motion.vercel.app/',
     site_name: 'Britt Connects',
+    images: [
+      {
+        url: '/images/social.jpg',
+        width: 1200,
+        height: 675,
+        alt: `Britt Connects Logo`,
+      },
+    ],
   },
   twitter: {
     handle: '@samuelgoddard',
